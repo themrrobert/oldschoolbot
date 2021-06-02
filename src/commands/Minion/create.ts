58b680642100f784d779df3548ef7232ceb44e00
themrrobert <10122432+themrrobert@users.noreply.github.com>
2021-06-01 17:36:35 -0700

    testing finished

--- src/commands/Minion/create.ts
+++ src/commands/Minion/create.ts
@@ -14,6 +14,7 @@ import {
 	removeBankFromBank,
 	stringMatches
 } from '../../lib/util';
+import getOSItem from "../../lib/util/getOSItem";
 
 export default class extends BotCommand {
 	public constructor(store: CommandStore, file: string[], directory: string) {
